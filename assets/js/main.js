b82f7d39281630dd93df8912a320de8533aab724
bandiiskola <nandor.bere@gmail.com>
2023-09-19 11:20:39 +0200

    Kész

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-var tomb=[]
-var ossz=0
+var tomb = []
+var ossz = 0
 
-function Mentes()
-{
-  if
+function Mentes() {
+  ossz = 0
+  if (document.getElementById("szel").value != "" && document.getElementById("hossz") != "") {
+    var obj = {
+      szelesseg: parseInt(document.getElementById("szel").value),
+      hosszusag: parseInt(document.getElementById("hossz").value)
+    }
+    tomb.push(obj)
+    for (var i = 0; i < tomb.length; i++) {
+      ossz += tomb[i].hosszusag * tomb[i].szelesseg
+    }
+    document.getElementById("beiras").innerHTML = "Az összterület: " + ossz
+  } else {
+    alert("Töltsd ki az adatokat!")
+  }
+
 }
